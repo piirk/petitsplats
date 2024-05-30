756d8356688beedf59f2d5a11230ca45f51a8d4e
piirk <baptiste.boucharechas@gmail.com>
2024-05-30 22:45:14 +0200

    mainSearch display results and refacto

--- js/models/Recipe.js
+++ js/models/Recipe.js
@@ -1,74 +1,74 @@
 class Recipe {
   constructor(data) {
-    this._id = data.id
-    this._image = data.image
-    this._name = data.name
-    this._description = data.description
+    this._id = data.id;
+    this._image = data.image;
+    this._name = data.name;
+    this._description = data.description;
     this._ingredients = data.ingredients.map(ingredient => { return new Ingredient(ingredient.ingredient, ingredient.quantity, ingredient.unit) });
-    this._time = data.time
-    this._servings = data.servings
-    this._appliance = data.appliance
-    this._ustensils = data.ustensils
+    this._time = data.time;
+    this._servings = data.servings;
+    this._appliance = data.appliance;
+    this._ustensils = data.ustensils;
   }
 
   get id() {
-    return this._id
+    return this._id;
   }
 
   get image() {
-    return this._image
+    return this._image;
   }
 
   get name() {
-    return this._name
+    return this._name;
   }
 
   get description() {
-    return this._description
+    return this._description;
   }
 
   get ingredients() {
-    return this._ingredients
+    return this._ingredients;
   }
 
   get time() {
-    return this._time
+    return this._time;
   }
 
   get servings() {
-    return this._servings
+    return this._servings;
   }
 
   get appliance() {
-    return this._appliance
+    return this._appliance;
   }
 
   get ustensils() {
-    return this._ustensils
+    return this._ustensils;
   }
 
   // Method to search a recipe by a query
   search(query) {
-    return this._name.toLowerCase().includes(query.toLowerCase())
+    return this._name.toLowerCase().includes(query.toLowerCase());
   }
 
   // Method to search a recipe by an ingredient
   searchIngredient(query) {
-    return this._ingredients.some(ingredient => ingredient.name.toLowerCase().includes(query.toLowerCase()))
+    return this._ingredients.some(ingredient => ingredient.name.toLowerCase().includes(query.toLowerCase()));
   }
 
   // Method to search a recipe by an appliance
   searchAppliance(query) {
-    return this._appliance.toLowerCase().includes(query.toLowerCase())
+    return this._appliance.toLowerCase().includes(query.toLowerCase());
   }
 
   // Method to search a recipe by an ustensil
   searchUstensil(query) {
-    return this._ustensils.some(ustensil => ustensil.toLowerCase().includes(query.toLowerCase()))
+    return this._ustensils.some(ustensil => ustensil.toLowerCase().includes(query.toLowerCase()));
   }
 
   // Method to search a recipe by description
   searchDescription(query) {
-    return this._description.toLowerCase().includes(query.toLowerCase())
+    return this._description.toLowerCase().includes(query.toLowerCase());
   }
 }
--- js/pages/index.js
+++ js/pages/index.js
@@ -1,33 +1,32 @@
 class IndexApp {
   constructor(recipes) {
-    this._recipes = recipes.map(recipe => { return new Recipe(recipe) })
+    this._recipes = recipes.map(recipe => { return new Recipe(recipe) });
   }
 
   get recipes() {
-      return this._recipes
+      return this._recipes;
   }
 
   init() {
     this.displayRecipes();
-
     this.addListenersMainSearch();
   }
 
-  displayRecipes() {
+  displayRecipes(recipes = this._recipes) {
     let recipesContainer = '';
-
-    this._recipes.forEach(recipe => {
+    recipes.forEach(recipe => {
       const recipeTemplate = new RecipeTemplate(recipe);
       recipesContainer += recipeTemplate.render();
     });
-
     document.getElementById('recipesContainer').innerHTML = recipesContainer;
   }
 
   addListenersMainSearch() {
-    document.getElementById('mainSearchInput').addEventListener('input', () => {
-      // if the search input is less than 3 characters, do nothing
+    const searchInput = document.getElementById('mainSearchInput');
+    searchInput.addEventListener('input', () => {
+      // if the search input is less than 3 characters, display all recipes
       if (searchInput.value.length < 3) {
+        this.displayRecipes();
         return;
       }
     
@@ -39,8 +38,7 @@ class IndexApp {
         }
       })
     
-      // display recipes cards: todo
-      console.log(searchResults);
+      this.displayRecipes(searchResults);
     });
   }
 
@@ -48,20 +46,20 @@ class IndexApp {
     let ingredients = [];
     this._recipes.forEach(recipe => {
       recipe.ingredients.forEach(ingredient => {
-        ingredients.push(ingredient)
-      })
-    })
-    return ingredients
+        ingredients.push(ingredient);
+      });
+    });
+    return ingredients;
   }
 
   getUstensils() {
     let ustensils = [];
     this._recipes.forEach(recipe => {
       recipe.ustensils.forEach(ustensil => {
-        ustensils.push(ustensil)
-      })
-    })
-    return ustensils
+        ustensils.push(ustensil);
+      });
+    });
+    return ustensils;
   }
 }
 
