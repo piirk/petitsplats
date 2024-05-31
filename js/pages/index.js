fdb6bf237fcd7df7e63ae5683cfe397d581a3df1
piirk <baptiste.boucharechas@gmail.com>
2024-05-31 09:39:38 +0200

    plural recipes counter

--- js/pages/index.js
+++ js/pages/index.js
@@ -19,7 +19,7 @@ class IndexApp {
       recipesContainer += recipeTemplate.render();
     });
     document.getElementById('recipesContainer').innerHTML = recipesContainer;
-    document.getElementById('recipesCount').innerText = recipes.length + ' recettes';
+    document.getElementById('recipesCount').innerText = recipes.length + (recipes.length > 1 ? ' recettes' : ' recette');
   }
 
   addListenersMainSearch() {
