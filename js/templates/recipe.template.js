8764cfb0a1e07f9dfd881579a020e28936b90651
piirk <baptiste.boucharechas@gmail.com>
2024-05-31 12:37:43 +0200

    switch to sass, updates classes and fixes

--- js/templates/recipe.template.js
+++ js/templates/recipe.template.js
@@ -8,20 +8,20 @@ class RecipeTemplate {
     return `
       <div class="card font-family-manrope">
         <img src="assets/image/recipe/${this._recipe.image}" class="card-img-top" alt="${this._recipe.name}">
-        <span class="badge">${this._recipe.time}min</span>
+        <span class="badge card-badge">${this._recipe.time}min</span>
         <div class="card-body d-flex flex-column">
-          <h2 class="card-title font-family-anton mt-3">${this._recipe.name}</h2>
+          <h2 class="card-title mt-3">${this._recipe.name}</h2>
           <div>
-            <h3 class="text-uppercase">Recette</h3>
-            <p class="card-text-description">${this._recipe.description}</p>
+            <h3 class="card-subtitle text-uppercase">Recette</h3>
+            <p class="card-text card-text-description">${this._recipe.description}</p>
           </div>
           <div>
-            <h3 class="text-uppercase">ingrédients</h3>
+            <h3 class="card-subtitle text-uppercase">ingrédients</h3>
             <div class="card-text">
               <ul class="ingredients-list list-group d-flex flex-row flex-wrap">
                 ${this._recipe.ingredients.map(ingredient => {
                   return `
-                    <li class="list-group-item">${ingredient.name}<br /><span class="ingredient-quantity">${ingredient.quantity + ' ' + ingredient.unit}</span></li>
+                    <li class="list-group-item">${ingredient.name}<br /><span class="ingredients-list__quantity">${ingredient.quantity + ' ' + ingredient.unit}</span></li>
                   `}).join('')}
               </ul>
             </div>
