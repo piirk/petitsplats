155203cb70a6d97ae58d8acfc378b38ef08797fa
piirk <baptiste.boucharechas@gmail.com>
2024-07-31 17:12:09 +0200

    refactor: Update AdvancedSearchSelectModel to set tabindex based on dropdown state

--- js/models/AdvancedSearchSelectModel.js
+++ js/models/AdvancedSearchSelectModel.js
@@ -185,7 +185,11 @@ class AdvancedSearchSelect {
       this._optionsList.forEach(option => {
         if (option.textContent.toLowerCase().includes(searchValue) && this._selectedOptions.indexOf(option.textContent) === -1){
           option.classList.remove('hide');
-          option.setAttribute('tabindex', '0');
+          if (this._isDropdownOpen) {
+            option.setAttribute('tabindex', '0');
+          } else {
+            option.setAttribute('tabindex', '-1');
+          }
         } else {
           option.classList.add('hide');
           option.setAttribute('tabindex', '-1');
